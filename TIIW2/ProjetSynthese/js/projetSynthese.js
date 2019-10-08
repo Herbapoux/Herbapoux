a7f3d8c0049a54ae459c054729da6e2caa777f91
Herbapoux <yannick-poulin@hotmail.com>
2019-10-08 14:05:36 -0400

    Nos Cours

--- TIIW2/ProjetSynthese/js/projetSynthese.js
+++ TIIW2/ProjetSynthese/js/projetSynthese.js
@@ -162,3 +162,325 @@ $('#canardHide').mouseleave(function () {
     $('#canardHide').hide();
     $('#canardShow').show();
 });
+
+//Tortue de mer
+
+$("#tortueShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#tortueHide').show();
+    $('#tortueShow').hide();
+});
+
+$('#tortueHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#tortueHide').hide();
+    $('#tortueShow').show();
+});
+
+//Loutre de mer
+
+$("#loutreShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#loutreHide').show();
+    $('#loutreShow').hide();
+});
+
+$('#loutreHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#loutreHide').hide();
+    $('#loutreShow').show();
+});
+
+//Salamandre
+
+$("#salamandreShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#salamandreHide').show();
+    $('#salamandreShow').hide();
+});
+
+$('#salamandreHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#salamandreHide').hide();
+    $('#salamandreShow').show();
+});
+
+//Poisson-lune
+
+$("#poissonShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#poissonHide').show();
+    $('#poissonShow').hide();
+});
+
+$('#poissonHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#poissonHide').hide();
+    $('#poissonShow').show();
+});
+
+//Poisson-lune
+
+$("#crocodileShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#crocodileHide').show();
+    $('#crocodileShow').hide();
+});
+
+$('#crocodileHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#crocodileHide').hide();
+    $('#crocodileShow').show();
+});
+
+//Baleine
+
+$("#baleineShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#baleineHide').show();
+    $('#baleineShow').hide();
+});
+
+$('#baleineHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#baleineHide').hide();
+    $('#baleineShow').show();
+});
+
+//Junior 3
+
+$("#niveau3Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau3Hide').show();
+    $('#niveau3Show').hide();
+});
+
+$('#niveau3Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau3Hide').hide();
+    $('#niveau3Show').show();
+});
+
+//Junior 4
+
+$("#niveau4Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau4Hide').show();
+    $('#niveau4Show').hide();
+});
+
+$('#niveau4Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau4Hide').hide();
+    $('#niveau4Show').show();
+});
+
+//Junior 5
+
+$("#niveau5Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau5Hide').show();
+    $('#niveau5Show').hide();
+});
+
+$('#niveau5Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau5Hide').hide();
+    $('#niveau5Show').show();
+});
+
+//Junior 6
+
+$("#niveau6Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau6Hide').show();
+    $('#niveau6Show').hide();
+});
+
+$('#niveau6Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau6Hide').hide();
+    $('#niveau6Show').show();
+});
+
+//Junior 7
+
+$("#niveau7Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau7Hide').show();
+    $('#niveau7Show').hide();
+});
+
+$('#niveau7Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau7Hide').hide();
+    $('#niveau7Show').show();
+});
+
+//Junior 8
+
+$("#niveau8Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau8Hide').show();
+    $('#niveau8Show').hide();
+});
+
+$('#niveau8Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau8Hide').hide();
+    $('#niveau8Show').show();
+});
+
+//Junior 9
+
+$("#niveau9Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau9Hide').show();
+    $('#niveau9Show').hide();
+});
+
+$('#niveau9Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau9Hide').hide();
+    $('#niveau9Show').show();
+});
+
+//Junior 10
+
+$("#niveau10Show").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau10Hide').show();
+    $('#niveau10Show').hide();
+});
+
+$('#niveau10Hide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#niveau10Hide').hide();
+    $('#niveau10Show').show();
+});
+
+//AQUAFORME
+
+//Aérobique
+
+$("#aerobiqueShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#aerobiqueHide').show();
+    $('#aerobiqueShow').hide();
+});
+
+$('#aerobiqueHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#aerobiqueHide').hide();
+    $('#aerobiqueShow').show();
+});
+
+//Spécialisée
+
+$("#specialiseeShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#specialiseeHide').show();
+    $('#specialiseeShow').hide();
+});
+
+$('#specialiseeHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#specialiseeHide').hide();
+    $('#specialiseeShow').show();
+});
+
+//Prénataux
+
+$("#prenatauxShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#prenatauxHide').show();
+    $('#prenatauxShow').hide();
+});
+
+$('#prenatauxHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#prenatauxHide').hide();
+    $('#prenatauxShow').show();
+});
+
+//Cardio-bébé
+
+$("#bebeShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#bebeHide').show();
+    $('#bebeShow').hide();
+});
+
+$('#bebeHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#bebeHide').hide();
+    $('#bebeShow').show();
+});
+
+//Cardio-secours
+
+$("#secoursShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#secoursHide').show();
+    $('#secoursShow').hide();
+});
+
+$('#secoursHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#secoursHide').hide();
+    $('#secoursShow').show();
+});
+
+//Soins immédiats
+
+$("#soinsShow").hover(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#soinsHide').show();
+    $('#soinsShow').hide();
+});
+
+$('#soinsHide').mouseleave(function () {
+    $('.containerFiltre').css("cursor", "pointer");
+
+    $('#soinsHide').hide();
+    $('#soinsShow').show();
+});
