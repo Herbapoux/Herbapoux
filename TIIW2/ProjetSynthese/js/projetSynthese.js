9755e8173acd0d5f03c7af9ddea5d02d11d53b91
Herbapoux <yannick-poulin@hotmail.com>
2019-10-11 11:53:52 -0400

    changment

--- TIIW2/ProjetSynthese/js/projetSynthese.js
+++ TIIW2/ProjetSynthese/js/projetSynthese.js
@@ -1,3 +1,13 @@
+//Test
+
+$('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+});
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+});
+
 
 //Natation
 
